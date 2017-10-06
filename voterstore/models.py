7f49fa13807435b34b4d6ae2d4b78b86f8477d03
maximzasorin <maximzasorin@gmail.com>
2017-10-06 09:31:06 +0300

    add tests

--- voterstore/models.py
+++ voterstore/models.py
@@ -12,7 +12,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)\
+            and self.pub_date < timezone.now()
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
